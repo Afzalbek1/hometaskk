58d9dcbea626cd080682c6336a78af7e8864bd48
Afzalbek Usonov <uafzalbek4@gmail.com>
2026-05-02 13:15:56 +0500

    add tests pagination thruttling

--- config/books/tests.py
+++ config/books/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .models import Category, Book
 
-# Create your tests here.
+class BookModelTest(TestCase):
+
+    def setUp(self):
+        self.category = Category.objects.create(
+            name="Test Category",
+            description="Test desc"
+        )
+
+        self.book = Book.objects.create(
+            category=self.category,
+            title="Test Book",
+            author="Test Author",
+            description="Test desc",
+            price=100,
+            is_active=True
+        )
+
+    def test_all_books(self):
+        books = Book.objects.all()
+        self.assertEqual(books.count(), 1)
+
+    def test_single_book(self):
+        book = Book.objects.get(id=self.book.id)
+        self.assertEqual(book.title, "Test Book")
+        self.assertEqual(book.author, "Test Author")
